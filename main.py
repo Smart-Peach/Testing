708a9ff9ed19df7a730a708ef52911616ecf3789
Taisia Kashintseva <the_smartest_peach@mail.ru>
2023-02-19 23:51:57 +0700

    seventh

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
 print(5 + 3)
 print(4 / 0)
 print(5 / 8)
-print(6 - 9)
-print(4 * 5)
-print('Hello')
-print('bla bla bla')
-print('It is ok')
-g = '1234'
-s = '21423452354'
-print(g + s)
+d = 5
+f = 6
+print(f - d)
